acbbcb90c17a3589cd30bc8ab5315b5d39a83346
Alexander Levchuk <alevchuk93@gmail.com>
2016-09-21 13:54:34 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,45 +1,21 @@
 var http = require('http');
-var fs = require('fs');
-var formidable = require("formidable");
-var util = require('util');
+var url = require("url");
+var frmprc = require("./formproc");
 
-var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() == 'get') {
-        displayForm(res);
-    } else if (req.method.toLowerCase() == 'post') {
-        processAllFieldsOfTheForm(req, res);
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received"); 
+
+    if (request.method.toLowerCase() == 'get') {
+       route(handle, pathname, request, response);
+    } else if (request.method.toLowerCase() == 'post') {
+       frmprc.process(request, response);
     }
+  }
 
-});
+  http.createServer(onRequest).listen(1185);
+  console.log("server listening on 1185");
 
-function displayForm(res) {
-    fs.readFile('contact.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-                'Content-Length': data.length
-        });
-        res.write(data);
-        res.end();
-    });
-}
-
-function processAllFieldsOfTheForm(req, res) {
-    var form = new formidable.IncomingForm();
-
-    form.parse(req, function (err, fields, files) {
-        //Store the data from the fields in your data store.
-        //The data store could be a file or database or any other store based
-        //on your application.
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('received the data:\n\n');
-        res.end(util.inspect({
-            fields: fields,
-            files: files
-        }));
-    });
-}
-
-server.listen(1185);
-console.log("server listening on 1185");
+};
+exports.start = start;
